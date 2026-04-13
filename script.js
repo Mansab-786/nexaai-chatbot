67b1ecd93e26d0c1e9c8b54a5f93ae2896d32cac
Mansab Ali <149017562+Mansab-786@users.noreply.github.com>
2026-04-13 09:59:13 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,1 @@
-async function sendMessage() {
-  let input = document.getElementById("input").value;
-  let messages = document.getElementById("messages");
 
-  messages.innerHTML += "<div><b>You:</b> " + input + "</div>";
-
-  let res = await fetch("/api/chat", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ message: input })
-  });
-
-  let data = await res.json();
-
-  messages.innerHTML += "<div><b>AI:</b> " + data.reply + "</div>";
-}
